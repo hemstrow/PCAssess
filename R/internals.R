d709e5f5df40981151b05aaaed3b00669e7fe321
hemstrow <hemstrow@gmail.com>
2024-11-15 16:08:42 -0800

    de-snpR'd the project. Needs testing.

---
+++ R/internals.R
@@ -0,0 +1,177 @@
+.boot_as <- function(x, n, facet){
+  ..tm <- ..ord <-  NULL
+
+  out <- vector("list", n)
+
+  shuff_ord <- out
+
+  # we could do all of this vectorized... but it would get very memory intense. Loop instead.
+  for(i in 1:n){
+    fshuf <- sample(facet, length(facet), FALSE)
+    out[[i]] <- prep_as_from_sn(x, fshuf)
+    out[[i]]$amat[,ho := out[[i]]$ho]
+    out[[i]]$facet <- fshuf
+  }
+
+  return(out)
+}
+
+.fix..call <- function(fun){
+  return(.suppress_specific_warning(fun, "variable in calling scope for clarity"))
+}
+
+.per_all_f_stat_components <- function(intot = NULL, jntot = NULL, ps1 = NULL, ps2 = NULL, r, nbar, nc, iho = NULL, jho = NULL, ntotm = NULL, psm = NULL, hom = NULL){
+  if(r == 2 & !is.null(intot)){
+    pbar <- ((intot*ps1) + (jntot*ps2))/(r*nbar) #average sample allele frequency
+    ssq <- (((intot)*(ps1-pbar)^2) + ((jntot)*(ps2-pbar)^2))/((r-1)*nbar) #sample variance of allele frequencies
+    hbar <- ((intot*iho) + (jntot*jho))/(r*nbar) #average heterozygote frequencies
+  }
+  else if (r == 1){
+    pbar <- ps1 #average sample allele frequency
+    ssq <- 0 #sample variance of allele frequencies
+    hbar <- iho #average heterozygote frequencies
+  }
+  else if(r > 2 | (r == 2 & is.null(intot))){
+    pbar <- rowSums(ntotm * psm)/(r*nbar) #average sample allele frequency
+    ssq <- rowSums(ntotm*(psm-pbar)^2)/((r - 1)*nbar)
+    hbar <- rowSums(ntotm*hom)/(r*nbar)
+  }
+
+
+
+  #equation parts used in both
+  inner1 <- pbar*(1-pbar)
+  inner2 <- ((r-1)/r)*ssq
+  inner3 <- .25*hbar
+
+  inner4 <- ((2*nbar - 1)/(4*nbar))*hbar
+  a <- (nbar/nc) * (ssq - (1/(nbar - 1))*(inner1 - inner2 - inner3))
+  b <- (nbar/(nbar-1))*(inner1 - inner2 - inner4)
+  c <- .5*hbar
+
+  # # browser()
+  # if(.print & !is.null(intot)){
+  #   saveRDS(data.frame(pbar = pbar, ssq = ssq, hbar = hbar, iho = iho, intot = intot, jntot = jntot, jho = jho, inner1 = inner1, inner2 = inner2, inner3 = inner3, inner4 = inner4, a = a, b = b, c = c), "test.RDS")
+  # }
+  # else if(.print){browser()}
+  return(list(a = a, b = b, c = c))
+
+  # weir--exactly the same
+  # else{
+  #   S1 <- ssq - (1/(nbar-1))*(inner1 - inner2 - inner3)
+  #   S2i1 <- ((r*(nbar - nc))/nbar)*inner1
+  #   S2i2 <- (1/nbar)*((nbar-1)+(r-1)*(nbar-nc))*ssq
+  #   S2i3 <- ((nbar-nc)/(4*nc^2))*hbar
+  #   S2 <- inner1 - (nbar/(r*(nbar-1)))*(S2i1 -S2i2 - S2i3)
+  #   return(list(S1 = S1, S2 = S2))
+  # }
+
+
+}
+
+
+
+
+.do_boots <- function(x, genotypes, par = FALSE, fst_cut = .95, store_pca){
+
+  nboots <- length(x)
+  if(par > 2){
+    cl <- parallel::makePSOCKcluster(par)
+    doParallel::registerDoParallel(cl)
+    out <- foreach::foreach(q = 1:nboots, .export = c("generate_summary_stats", "global_fst", "quick_smartPCA"),
+                            .packages = c("data.table"), .inorder = FALSE) %dopar% {
+                              tdata <- x[[q]]
+
+                              tout <- generate_summary_stats(as = tdata$amat, genotypes = genotypes, facet = tdata$facet, fst_cut = fst_cut, store_pca = store_pca)
+
+                              if(store_pca){
+                                tout <- list(res = data.table(boot = q, Fstat = tout$Fstat, fst = tout$fst,
+                                                              delta_Fstat = tout$delta_Fstat, delta_fst = tout$delta_fst,
+                                                              init_Fstat = tout$init_Fstat, init_fst = tout$init_fst),
+                                             pca = tout$pca)
+                              }
+                              else{
+                                tout <- data.table(boot = q, Fstat = tout$Fstat, fst = tout$fst, delta_Fstat = tout$delta_Fstat, delta_fst = tout$delta_fst, init_Fstat = tout$init_Fstat, init_fst = tout$init_fst)
+                              }
+                              tout
+                            }
+
+    parallel::stopCluster(cl)
+
+
+    if(store_pca){
+      pca <- purrr::map(out, "pca")
+      out <- data.table::rbindlist(purrr::map(out, "res"))
+      out <- dplyr::arrange(out, boot)
+
+      out <- list(res = out, pca = pca)
+    }
+    else{
+      out <- data.table::rbindlist(out)
+      out <- dplyr::arrange(out, boot)
+    }
+  }
+  else{
+    out <- data.table(boot = 1:nboots)
+    out$Fstat <- 0
+    out$fst <- 0
+    out$delta_Fstat <- 0
+    out$delta_fst <- 0
+    out$init_Fstat <- 0
+    out$init_fst <- 0
+
+    if(store_pca){
+      pca <- vector("list", nboots)
+    }
+
+    browser()
+    for(i in 1:nboots){
+      cat(i, "\n")
+
+
+      tout <- generate_summary_stats(as = x[[i]]$amat, genotypes = genotypes, facet = x[[i]]$facet, fst_cut = fst_cut)
+      if(store_pca){
+        pca[[i]] <- tout$pca
+      }
+
+      out[i, Fstat := tout$Fstat]
+      out[i, fst := tout$fst]
+      out[i, delta_Fstat := tout$delta_Fstat]
+      out[i, delta_fst := tout$delta_fst]
+      out[i, init_Fstat := tout$init_Fstat]
+      out[i, init_fst := tout$init_fst]
+    }
+
+    if(store_pca){
+      out <- list(res = out, pca = pca)
+    }
+  }
+  return(out)
+}
+
+.prep_boots <- function(x, facet, n){
+  x <- as.data.table(x)
+  as <- prep_as_from_sn(x, facet)
+
+  as_real <- as$amat
+  as_real[,ho := as$ho]
+
+  boots <- .boot_as(x, n, facet)
+
+  return(list(real_as = as_real, boot_as = boots))
+}
+
+.suppress_specific_warning <- function(fun, warn_to_suppress){
+  warn_handle <- function(warn){
+    if(any(grepl(warn_to_suppress, warn))){
+      invokeRestart("muffleWarning")
+    }
+  }
+
+  withCallingHandlers(res <- fun,
+                      warning = warn_handle)
+
+
+  return(res)
+}
+
---
+++ R/utilities.R
@@ -0,0 +1,190 @@
+#' @param x allele-count and observed heterozygosity data. Required columns are
+#'   \code{variable} containing locus IDs, \code{vf} containing
+#'   population/facet IDs, two containing allele counts for
+#'   the major/ref and minor/alt loci, respectively, and \code{ho} for observed
+#'   heterozygosity. Note that this object can be created from 0/1/2 formatted
+#'   genotypic data with \code{prep_as_from_sn}.
+#' @param ac_cols character, default c("0", "1"). Column names for the two
+#'   alleles in x.
+#'
+#' @export
+global_fst <- function(x, ac_cols = c("0", "1")){
+  ac_cols <- ac_cols[which(ac_cols %in% colnames(x))]
+  nt <- data.table::dcast(x[,rowSums(.SD), .SDcols = ac_cols, by = .(vf, variable)], variable ~ vf, value.var = "V1")
+  psm <- x[,.SD/rowSums(.SD), .SDcols = ac_cols, by = .(vf, variable)]
+  ntotm <- nt[,-1]/2
+  ntotm <- cbind(nt[,1], ntotm)
+
+  hom <- data.table::dcast(x, variable ~ vf, value.var = "ho")
+
+  pops <- unique(x[,vf])
+  r <- length(pops) # number of comps
+  nbar <- rowMeans(ntotm[,-1]) #average sample size in individuals
+  CV <- matrixStats::rowSds(as.matrix(ntotm[,-1]))/nbar # coefficient of variation in sample size
+  nc <- nbar*(1-(CV^2)/r)
+  parts <- vector("list", length(ac_cols))
+
+  out <- data.table::data.table(variable = sort(unique(x$variable)),
+                                fst = 0,
+                                a = 0,
+                                b = 0,
+                                c = 0)
+
+  for(k in 1:length(ac_cols)){
+    psf_m <- dcast(psm, variable ~ vf, value.var = colnames(psm)[k + 2])
+
+    # need to determine per-allele hom
+    # if(!bi_allelic){
+    #   het_cols_containing_k <- which(xor(gc_colnames_1 == colnames(ps1_f)[k], gc_colnames_2 == colnames(ps1_f)[k])) # hets (xor) with this allele
+    #   tiho <- .fix..call(rowSums(idat[,..gc_cols][,..het_cols_containing_k])/intot)
+    #   tjho <- .fix..call(rowSums(jdat[,..gc_cols][,..het_cols_containing_k])/jntot)
+    # }
+
+    # otherwise we have the ho already
+    absent <- which(rowSums(psf_m[,-1]) == 0)
+    thom <- data.table::copy(hom)
+    if(length(absent) != 0){
+      data.table::set(thom, absent, 2:ncol(thom), value = 0)
+    }
+
+    parts[[k]] <- .per_all_f_stat_components(ntotm = ntotm[,-1], psm = psf_m[,-1], r = r, nbar = nbar, nc = nc, hom = thom[,-1])
+  }
+
+  a <- matrix(unlist(purrr::map(parts, "a")), ncol = length(parts))
+  b <- matrix(unlist(purrr::map(parts, "b")), ncol = length(parts))
+  c <- matrix(unlist(purrr::map(parts, "c")), ncol = length(parts))
+
+  data.table::set(out, j = "a", value = rowSums(a)) # write a
+  data.table::set(out, j = "b", value = rowSums(b)) # write b
+  data.table::set(out, j = "c", value = rowSums(c)) # write v
+  # Fst <- rowSums(a)/rowSums(a + b + c)
+
+  out[,fst := a/(a + b + c)]
+  meta_colnames <- colnames(x)[1:(which(colnames(x) %in% ac_cols)[1] - 1)]
+  out$nk <- rowSums(nt[,-1])
+
+
+  means <- out[,list(weighted_fst = stats::weighted.mean(a, w = nk, na.rm = T)/
+                       stats::weighted.mean(a + b + c, w = nk, na.rm = T),
+                     fst = mean(a, na.rm = TRUE)/mean(a + b + c, na.rm = TRUE))]
+
+  return(list(means = means, pairwise = out))
+}
+
+#' @param x genotypic data, formatted with genotypes as 0/1/2 for the
+#'   homozygous ref/major, heterozygous, and homozygous alt/minor, respectively.
+#'   Each row must be one locus, each column one individual. Column names must
+#'   be unique.
+#'
+#' @export
+quick_smartPCA <- function(sn){
+  ms <- colMeans(sn, na.rm = TRUE)
+  ms <- ms[col(sn)]
+  afs <- (1+ms)/(2 + 2*nrow(sn)) # adjusted eqn, from Price et al 2006
+  # afs <- ms/2 # according to patterson
+  sn <- (sn - ms)/sqrt(afs*(1-afs)) # eqn 3, patterson et al 2006
+  sn[is.na(sn)] <- 0
+
+  pca_r <- svd(sn)
+  colnames(pca_r$u) <- paste0("PC", 1:ncol(pca_r$u))
+
+  return(pca_r$u)
+}
+
+#' @param x allele-count and observed heterozygosity data. Required columns are
+#'   \code{variable} containing locus IDs, \code{vf} containing
+#'   population/facet IDs, two containing allele counts for
+#'   the major/ref and minor/alt loci, respectively, and \code{ho} for observed
+#'   heterozygosity. Note that this object can be created from 0/1/2 formatted
+#'   genotypic data with \code{prep_as_from_sn}.
+#' @param x genotypic data, formatted with genotypes as 0/1/2 for the
+#'   homozygous ref/major, heterozygous, and homozygous alt/minor, respectively.
+#'   Each row must be one locus, each column one individual. Column names must
+#'   be unique.
+#' @param facet character vector noting population informtion for each
+#'   inidivudal sample.
+#' @param ac_cols character, default c("0", "1"). Column names for the two
+#'   alleles in as.
+#' @param fst_cut numeric, default 0.95. The Fst quantile above which to label
+#'   loci as high-Fst.
+#' @param store_pca logical, default FALSE. If TRUE, PCAs for both all and high-
+#'   Fst loci will be retained and returned.
+#'
+#' @export
+generate_summary_stats <- function(as, genotypes, facet, ac_cols = c("0", "1"), fst_cut = .95,
+                                   store_pca = FALSE){
+  fst <- global_fst(as, ac_cols)
+  ofst <- fst$means$fst
+
+  opca <- quick_smartPCA(t(genotypes))
+  opca <- as.data.table(opca)
+  opca$pop <- facet
+  omav <- manova(cbind(PC1, PC2) ~ pop, opca)
+  omav <- summary(omav)$stats[1,"approx F"]
+
+
+  high_fst <- which(fst$pairwise$fst >= quantile(fst$pairwise$fst, fst_cut, na.rm = TRUE))
+  high_fst <- fst$pairwise$variable[high_fst]
+
+  high_fst_as <- as[which(as$variable %in% high_fst),]
+  fst_high <- global_fst(high_fst_as, ac_cols)
+  tfst <- fst_high$means$fst
+
+
+  tpca <- quick_smartPCA(t(genotypes[which(colnames(genotypes) %in% high_fst),]))
+
+  tpca <- as.data.table(tpca)
+  tpca$pop <- facet
+
+  mav <- manova(cbind(PC1, PC2) ~ pop, tpca)
+  mav <- summary(mav)$stats[1,"approx F"]
+
+  if(store_pca){
+    return(list(Fstat = mav, fst = tfst, delta_Fstat = mav - omav, delta_fst = tfst - ofst,
+                init_Fstat = omav, init_fst = ofst,
+                pca = list(all_vars = opca, selected = tpca)))
+  }
+  return(list(Fstat = mav, fst = tfst, delta_Fstat = mav - omav, delta_fst = tfst - ofst, init_Fstat = omav, init_fst = ofst))
+}
+#' @export
+get_p_values <- function(observed, null, h0 = c("less", "greater")){
+  ec_dists <- lapply(null, ecdf)
+
+  p <- numeric(length(observed))
+  names(p) <- names(observed)
+  for(i in 1:length(ec_dists)){
+    p[i] <- ec_dists[[i]](observed[[i]])
+    if(h0[i] == "greater"){
+      p[i] <- 1-p[i]
+    }
+  }
+  return(p)
+}
+
+#' @param x genotypic data, formatted with genotypes as 0/1/2 for the
+#'   homozygous ref/major, heterozygous, and homozygous alt/minor, respectively.
+#'   Each row must be one locus, each column one individual. Column names must
+#'   be unique.
+#' @param facet character vector noting population informtion for each
+#'   inidivudal sample.
+#'
+#' @export
+prep_as_from_sn <- function(x, facet){
+  browser()
+  samps <- colnames(x)
+  colnames(x) <- as.character(1:ncol(x))
+  x <- data.table::melt(data.table::transpose(x, keep.names = "samp"), id.vars = "samp") # transpose and melt
+  data.table::set(x, j = "vf", value = facet[as.numeric(x$samp)])
+
+  gmat <- data.table::dcast(data.table::setDT(x), variable + vf ~ value, value.var='value', length) # cast
+
+  ho <- gmat$`1`/(gmat$`0` + gmat$`2`)
+
+  amat <- gmat[,.(variable, vf)]
+  amat$`0` <- gmat$`0`*2 + gmat$`1`
+  amat$`1` <- gmat$`2`*2 + gmat$`1`
+
+  gmat$`NA` <- NULL
+
+  return(list(gmat = gmat, amat = amat, ho = ho))
+}
