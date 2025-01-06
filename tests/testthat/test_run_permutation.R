c8fa587b05a445db7eab70a2367c7873d11e5903
Andy Lee <andymuanlee@gmail.com>
2025-01-06 15:32:59 -0500

    create files for testing

---
+++ tests/testthat/test_run_permutation.R
@@ -0,0 +1,20 @@
+library(testthat)
+library(PCAssess)
+
+?PCAssess::run_permutation
+
+test_that("Check input files", (
+  expect_error(run_permutation(x, facet)))
+
+)
+
+test_that("check fst cut-offs", {
+  expect_error(run_permutation(fst_cut < 0))
+  expect_error(run_permutation(fst_cut > 1))
+})
+
+
+# expect_output(x, y)
+# expect_message(x, y)
+# expect_warning(x, y)
+# expect_error(x, y)
